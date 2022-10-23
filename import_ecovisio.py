574274163b8fa95714811490fb70743e92937ec7
Poschi <development@poschi.net>
2022-10-23 23:11:05 +0200

    Added timeout and set timeframe to 7 days

--- import_ecovisio.py
+++ import_ecovisio.py
@@ -51,7 +51,7 @@ def extract_points(bikecounter, from_day: datetime, to_day: datetime, interval: 
     print(f"Interval {interval}")
     print(url)
 
-    response = requests.get(url=url).json()
+    response = requests.get(url=url, timeout=60_000).json()
     if len(response) <= 0:
         print("No values found")
         return
@@ -84,13 +84,13 @@ def extract_points(bikecounter, from_day: datetime, to_day: datetime, interval: 
 
 influxdb = config.influxdb
 
-with InfluxDBClient(url=influxdb.server, token=influxdb.token, org=influxdb.org) as client:
+with InfluxDBClient(url=influxdb.server, token=influxdb.token, org=influxdb.org, timeout=60_000) as client:
     write_api = client.write_api(write_options=SYNCHRONOUS)
     for bikecounter in config.ecovisio:
         print()
         print(f"Bikecounter {bikecounter.country}-{bikecounter.city}-{bikecounter.location}")
         to_day = datetime.now() + timedelta(days=1)
-        from_day = to_day - timedelta(days=40)
+        from_day = to_day - timedelta(days=7)
 
         points_86400 = extract_points(bikecounter, from_day, to_day, Interval.DAY)
         write_api.write(influxdb.bucket, influxdb.org, points_86400)
